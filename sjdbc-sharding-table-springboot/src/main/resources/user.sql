95870bb361174d538670947d8acf191c609f89c3
li--1 <1448498478@qq.com>
2021-09-14 08:30:31 +0800

    jdbc

---
+++ sjdbc-sharding-table-springboot/src/main/resources/user.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE `ds_2` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+CREATE TABLE `user_0`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_1`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_2`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_3`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+#ds_0 库名 一次性删除库中所有表数据
+SELECT CONCAT('TRUNCATE table ',table_name,';') FROM information_schema.`TABLES` WHERE table_schema='ds_0';
